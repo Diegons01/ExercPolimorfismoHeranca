495c31979d5740367d3cae7794ceccee0b879f0f
Diego Nascimento <diegodnascimento01@gmail.com>
2019-02-03 16:36:53 -0200

    Classe Pronta

---
+++ ExercPolimorfismoHeranca/Entities/ImportedProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExercPolimorfismoHeranca.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomFree { get; set; }
+        private double _totalPrice = 0.0;
+        
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customFree) :base(name, price)
+        {
+            CustomFree = customFree;
+        }
+
+        public override string PriceTag()
+        {                       
+            return Name + "$" + TotalPrice() + "(Customs fee: $" + CustomFree.ToString("F2", CultureInfo.InvariantCulture) + ")";
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomFree;
+        }
+
+    }
+}
---
+++ ExercPolimorfismoHeranca/Entities/UsedProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercPolimorfismoHeranca.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManafacturedDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manafacturedDate) : base(name, price)
+        {
+            ManafacturedDate = manafacturedDate;
+        }
+
+        public override string PriceTag()
+        {
+            //Iphone (used) $ 400.00 (Manufacture date: 15/03/2017)
+            return  Name + "(used) $" + base.PriceTag() + "(Manufacute date: " + ManafacturedDate.ToString("ddMMyyy");
+        }
+    }
+}
